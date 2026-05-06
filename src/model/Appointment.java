ddddb0b81435779d6abfde1b26b7ae3a3053f542
Yağmur <yagmure.savas@gmail.com>
2026-05-07 00:21:00 +0300

    Clean up Appointment class constructors
    Removed commented-out constructors for Appointment class.

--- src/model/Appointment.java
+++ src/model/Appointment.java
@@ -8,18 +8,6 @@ private String vetName;
 private Animal patient;
 public String getDate() {return date;};
 public String getTime() {return time;};
-/*
-public Appointment(String date,String time,Object patient) { //girdi olarak alınan değerleri kendi belirlediğimiz değişkenlere atıyoruz
-this.date=date;
-this.time=time;
-this.patient=patient;
-}
-public Appointment(String date,String time,Object patient,String vetName) { //neden hepsi ayrı ayrı birtanesinde olmzmıydı
-	this.date=date;
-	this.time=time;
-	this.patient=patient;
-	this.vetName=vetName;
-}*/
 public Appointment(String date,String time,Animal patient,String vetName,String description) {
 	this.date=date;
 	this.time=time;
