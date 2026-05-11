8c507fa2b9402ef4d8c2529e51dc3713688060b2
Yağmur <yagmure.savas@gmail.com>
2026-05-11 19:44:15 +0300

    Add create method for Fish class with user input

--- src/model/Fish.java
+++ src/model/Fish.java
@@ -11,7 +11,44 @@ public class Fish extends Animal{
 			this.waterType=waterType;
 			this.color=color;
 		}
-		
+	public static Fish create(Scanner scanner, Owner owner) {
+	        System.out.print("Adı: ");
+	        String name = scanner.nextLine();
+	        System.out.print("Cinsi: ");
+	        String breed = scanner.nextLine();
+	        int age;
+			while (true) {
+			    System.out.print("Yaş: ");
+			    age = scanner.nextInt();
+			    if (age >= 0) break;
+			    System.out.println("Hata: Yaş negatif değer olamaz! Tekrar girin.");
+			}
+
+			double weight;
+			while (true) {
+			    System.out.print("Kilo: ");
+			    weight = scanner.nextDouble();
+			    if (weight >= 0) break;
+			    System.out.println("Hata: Kilo negatif olamaz! Tekrar girin.");
+			}
+
+			double height;
+			while (true) {
+			    System.out.print("Boy: ");
+			    height = scanner.nextDouble();
+			    if (height >= 0) break;
+			    System.out.println("Hata: Boy negatif olamaz! Tekrar girin.");
+			}
+	        scanner.nextLine();
+	        System.out.print("Su Tipi: ");
+	        String waterType = scanner.nextLine();
+	        System.out.print("Rengi: ");
+	        String color = scanner.nextLine();
+	 
+	        System.out.println("><((((º>");
+	 
+	        return new Fish(name, breed, age, weight, height, owner, waterType, color);
+	    }				
 	@Override
 	public String getType() {
 		return "Balık";
