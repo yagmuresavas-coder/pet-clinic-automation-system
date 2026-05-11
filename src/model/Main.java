ecacdfbcc1a1af77872652c01a5e56e974085a2c
Yağmur <yagmure.savas@gmail.com>
2026-05-11 19:22:46 +0300

    Refactor Main class and improve structure

--- src/model/Main.java
+++ src/model/Main.java
@@ -1,6 +1,5 @@
 package model;
 
-import sahiplendirme.Question;
 import sahiplendirme.Quiz;
 import sahiplendirme.ResultEvaluator;
 import staff.Veterinarian;
@@ -8,204 +7,178 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
+    static Scanner scanner = new Scanner(System.in);
+ 
+    
+    //Kullanıcıyı Önceden Kayıtlı tutmak için kullanıcı nesnesi oluşturuyoruz
+    static Owner owner = new Owner("Yağmur", "Rümişoğlu", "111-2222-3333", "feyza@mail.com",
+            new Address("Atatürk Cad.", "İstanbul", "34000"));
 
-		Scanner scanner = new Scanner(System.in);
-		Address address = new Address("Atatürk Cad.", "İstanbul", "34000");
-		Owner owner = new Owner(" Yağmur ", " Rümişoğlu ", " 111-2222-3333 ", " feyza@mail.com ", address);
-		
-		// 3 adet hazır Veteriner nesnesi oluşturuluyor
-		Veterinarian vet1 = new Veterinarian("Dr. Ali Yılmaz", "ali@mail.com", "555-111", "Cerrahi", new String[]{"Pazartesi", "Çarşamba", "Cuma"});
-		Veterinarian vet2 = new Veterinarian("Dr. Ayşe Kaya", "ayse@mail.com", "555-222", "Dahiliye", new String[]{"Salı", "Perşembe"});
-		Veterinarian vet3 = new Veterinarian("Dr. Mehmet Öz", "mehmet@mail.com", "555-333", "Göz Hastalıkları", new String[]{"Pazartesi", "Salı", "Cuma"});
-		Veterinarian[] vets = {vet1, vet2, vet3};
+    static Veterinarian[] vets = {
+        new Veterinarian("Dr. Ali Yılmaz",  "ali@mail.com",    "555-111", "Veteriner Cerrahi",         new String[]{"Pazartesi", "Çarşamba", "Cuma"}),//Doktorlarımızı ve Hastaya Bakabilecekleri günleri bir diziye kaydediyoruz
+        new Veterinarian("Dr. Ayşe Kaya",   "ayse@mail.com",   "555-222", "Veteriner Dahiliye",         new String[]{"Salı", "Perşembe"}),
+        new Veterinarian("Dr. Mehmet Öz",   "mehmet@mail.com", "555-333", "Veteriner Acil Tıp ve Yoğun Bakım", new String[]{"Pazartesi", "Salı", "Cuma"})
+    };
 
-		System.out.println(
-				"========= HANGİ SİSTEMİ TERCİH EDECEKSİNİZ ==========\n Evcil Hayvan Sahiplendirme[1]\n Veteriner Randevu Sistemi[2]\n Tıbbi Kayıt ve Aşı Takip Sistemi[3]");
-		int secim = scanner.nextInt();
-		scanner.nextLine(); // Satır sonu karakterini temizle
+    // Kaydedilen hayvanları tutan dizi
+    static Animal[] hayvanlar = new Animal[100];
+    static int hayvanSayisi = 0;
 
-		if (secim == 1) {
-			Quiz quiz = new Quiz();
-			quiz.start();
-			ResultEvaluator evaluator = new ResultEvaluator(quiz.getTotalScores());
-			evaluator.showResult();
-		} else if (secim == 2) {
-			System.out.println(
-					"##################################################################\n\tPETCLİNİC VETERİNER KLİNİĞİNE HOŞGELDİNİZ\n##################################################################");
-			System.out.println("HOŞGELDİNİZ: " + owner);
-			
-			System.out.println("\n--- KLİNİĞİMİZDEKİ VETERİNER HEKİMLER ---");
-			for (int i = 0; i < vets.length; i++) {
-				System.out.println((i + 1) + ". " + vets[i].getName() + " (" + vets[i].getSpecialization() + ") - Çalışma Günleri: " + String.join(", ", vets[i].getWorkingDays()));
-			}
-			System.out.print("Hangi Veterineri Seçmek İstersiniz? (1-3): ");
-			int vetChoice = scanner.nextInt();
-			scanner.nextLine();
-			String selectedVetName = (vetChoice >= 1 && vetChoice <= 3) ? vets[vetChoice - 1].getName() : "Bilinmiyor";
+    public static void main(String[] args) {
 
-			System.out.print("\nTür Seçin (1-Köpek, 2-Kedi, 3-Kuş, 4-Balık, 5-At): ");
-			int tur = scanner.nextInt();
-			scanner.nextLine(); // Satır sonu karakterini temizle
+        while (true) { // 3 farklı sistemimiz ve bir çıkış seçeneğimiz var.Kullanıcı istediği sistemi seçiyor ve seçilen döndüye giriliyor.
+            System.out.println("\n========= HANGİ SİSTEMİ TERCİH EDECEKSİNİZ ==========\n"
+                    + " Evcil Hayvan Sahiplendirme[1]\n"
+                    + " Veteriner Randevu Sistemi[2]\n"
+                    + " Tıbbi Kayıt ve Aşı Takip Sistemi[3]\n"
+                    + " Kayıtlı Hayvanları Göster[4]\n"
+                    + " Çıkış[0]");
+            int secim = scanner.nextInt();
+            scanner.nextLine();
 
-			// Geçersiz bir seçim yapıldı mı kontrolü
-			if (tur < 1 || tur > 5) {
-				System.out.println("Hata: Geçersiz tür seçtiniz!");
-				return; // Programı burada bitirir
-			}
+            if (secim == 1) {
+                sahiplendirmeSistemi();
+            } else if (secim == 2) {
+                randevuSistemi();
+            } else if (secim == 3) {
+                tibbikayitSistemi();
+            } else if (secim == 4) {
+                hayvanlarıGoster();
+            } else if (secim == 0) {
+                System.out.println("Programdan çıkılıyor. Güle güle!");//burada çıkış seçildiğinde döngü bitiyor pogram sonlanıyor
+                break;
+            } else {//eğer istenilen girdilerden farklı bir seçenek seçilirse hata verir ve sistem tekrar seçim yapılması için başa döner 
+                System.out.println("HATALI SEÇİM YAPTINIZ TEKRAR DENEYİNİZ!");
+                return;
+            }
+        }
 
-			Animal hayvan = createAnimal(scanner, owner, tur);
+        scanner.close();
+    }
 
-			System.out.println("\nRANDEVU OLUŞTURMA EKRANI");
-			System.out.println("Seçilen Veteriner: " + selectedVetName);
-			System.out.print("Tarih (Örn: 10.05.2026): ");
-			String date = scanner.nextLine();
+    //1. SİSTEM: Sahiplendirme 
+    private static void sahiplendirmeSistemi() { //sahiplendirme sisteminin içeriği
+        Quiz quiz = new Quiz(); //yeni bir quiz nesnesi oluşturuyoruz
+        quiz.start();//quizi başlatıryoruz
+        ResultEvaluator evaluator = new ResultEvaluator(quiz.getTotalScores());// quiz sonucuna göre bir hayvan belirtmek için sonuç sınıfından bir sonuç nesnesi oluşturuyoruz
+        evaluator.showResult();//hesaplama sonucunda çıkan hayvanı göstermek için method çağırıyoruz
+    }
 
-			System.out.print("Saat (Örn: 14:30): ");
-			String time = scanner.nextLine();
+    //2. SİSTEM: Veteriner Randevu
+    private static void randevuSistemi() { //randevu sisteminin içeriği 
+        System.out.println("##################################################################\n"
+                + "\tPETCLİNİC VETERİNER KLİNİĞİNE HOŞGELDİNİZ\n"
+                + "##################################################################");
+        System.out.println("HOŞGELDİNİZ: " + owner);
+        //sistemde kayıtlı olan veterinerleri gösteriyoruz
+        System.out.println("\n--- KLİNİĞİMİZDEKİ VETERİNER HEKİMLER ---");
+        for (int i = 0; i < vets.length; i++) { 
+            System.out.println((i + 1) + ". " + vets[i].getName()
+                    + " (" + vets[i].getSpecialization() + ")"
+                    + " - Çalışma Günleri: " + String.join(", ", vets[i].getWorkingDays()));
+        }
+        System.out.print("Hangi Veterineri Seçmek İstersiniz? (1-3): ");
+        int vetChoice = scanner.nextInt();
+        scanner.nextLine();
+        String selectedVetName = (vetChoice >= 1 && vetChoice <= 3) ? vets[vetChoice - 1].getName() : "Bilinmiyor";
 
-			System.out.print("Açıklama: ");
-			String description = scanner.nextLine();
+        System.out.print("\nTür Seçin (1-Köpek, 2-Kedi, 3-Kuş, 4-Balık, 5-At): ");
+        int tur = scanner.nextInt();
+        scanner.nextLine();
+        if (tur < 1 || tur > 5) {
+            System.out.println("Hata: Geçersiz tür seçtiniz!");
+            return;
+        }
 
-			Appointment randevu = new Appointment(date, time, hayvan, selectedVetName, description);
+        Animal hayvan = createAnimal(tur); //Animala extend edilen alt sınıflarda bulunan createanimal methoduyla hayvan nesnesini oluşturuyoruz.
 
-			System.out.println("YENİ RANDEVUNUZ: " + randevu);
-			
-		} else if (secim == 3) {
-			System.out.println(
-					"##################################################################\n\tTIBBİ KAYIT VE AŞI TAKİP SİSTEMİ\n##################################################################");
-			
-			System.out.print("Tür Seçin (1-Köpek, 2-Kedi, 3-Kuş, 4-Balık, 5-At): ");
-			int tur = scanner.nextInt();
-			scanner.nextLine(); // Satır sonu karakterini temizle
+        System.out.println("\n\n\n--- RANDEVU OLUŞTURMA EKRANI ---");
+        System.out.println("Seçilen Veteriner: " + selectedVetName);
+        System.out.print("Tarih (Örn: 10.05.2026): ");
+        String date = scanner.nextLine();
+        System.out.print("Saat (Örn: 14:30): ");
+        String time = scanner.nextLine();
+        System.out.print("Açıklama: ");
+        String description = scanner.nextLine();
 
-			if (tur < 1 || tur > 5) {
-				System.out.println("Hata: Geçersiz tür seçtiniz!");
-				return;
-			}
+        Appointment randevu = new Appointment(date, time, hayvan, selectedVetName, description);
+        System.out.println("YENİ RANDEVUNUZ: " + randevu);
+    }
 
-			Animal hayvan = createAnimal(scanner, owner, tur);
-			
-			System.out.print("\nBugünün Tarihi (Örn: 10.05.2026): ");
-			String date = scanner.nextLine();
+    // 3. SİSTEM: Tıbbi Kayıt
+    private static void tibbikayitSistemi() {
+        System.out.println("##################################################################\n"
+                + "\tTIBBİ KAYIT VE AŞI TAKİP SİSTEMİ\n"
+                + "##################################################################");
 
-			System.out.println("\n--- TIBBİ KAYIT BİLGİLERİ ---");
-			System.out.print("Hastalık Tanısı (Teşhis): ");
-			String diagnosis = scanner.nextLine();
-			
-			System.out.print("Uygulanan Tedavi: ");
-			String treatment = scanner.nextLine();
-			
-			MedicalRecord medicalRecord = new MedicalRecord(date, diagnosis, treatment, hayvan);
-			System.out.println("TIBBİ KAYIT BAŞARIYLA OLUŞTURULDU: " + medicalRecord);
-			
-			System.out.println("\n--- AŞI BİLGİLERİ ---");
-			System.out.print("Aşı Adı (Örn: Karma, Kuduz): ");
-			String vaccineName = scanner.nextLine();
-			
-			System.out.print("Bir Sonraki Aşı Tarihi (Örn: 10.05.2027): ");
-			String nextDueDate = scanner.nextLine();
-			
-			Vaccine vaccine = new Vaccine(vaccineName, date, nextDueDate, hayvan);
-			System.out.println("AŞI KAYDI BAŞARIYLA OLUŞTURULDU: " + vaccine);
-			
-		} else {
-			System.out.println("HATALI SEÇİM YAPTINIZ TEKRAR DENEYİNİZ!");
-		}
+        System.out.print("Tür Seçin (1-Köpek, 2-Kedi, 3-Kuş, 4-Balık, 5-At): ");
+        int tur = scanner.nextInt();
+        scanner.nextLine();
+        if (tur < 1 || tur > 5) {
+            System.out.println("Hata: Geçersiz tür seçtiniz!");
+            return;
+        }
 
-		scanner.close();
-	}
-	
-	// Hayvan yaratma işlemini ana metodu çok kalabalıklaştırmaması için ayrı bir metoda aldık.
-	private static Animal createAnimal(Scanner scanner, Owner owner, int tur) {
-		System.out.print("Adı: ");
-		String name = scanner.nextLine();
+        Animal hayvan = createAnimal(tur);
 
-		System.out.print("Cinsi: ");
-		String breed = scanner.nextLine();
+        System.out.print("\nBugünün Tarihi (Örn: 10.05.2026): ");
+        String date = scanner.nextLine();
 
-		System.out.print("Yaş: ");
-		int age = scanner.nextInt();
+        System.out.println("\n\n\n--- TIBBİ KAYIT BİLGİLERİ ---");
+        System.out.print("Hastalık Tanısı: ");
+        String diagnosis = scanner.nextLine();
+        System.out.print("Uygulanan Tedavi: ");
+        String treatment = scanner.nextLine();
 
-		System.out.print("Kilo: ");
-		double weight = scanner.nextDouble();
+        MedicalRecord medicalRecord = new MedicalRecord(date, diagnosis, treatment, hayvan);
+        System.out.println("TIBBİ KAYIT BAŞARIYLA OLUŞTURULDU: " + medicalRecord);
 
-		System.out.print("Boy: ");
-		double height = scanner.nextDouble();
-		scanner.nextLine(); // Satır sonu karakterini temizle
+        System.out.println("\n--- AŞI BİLGİLERİ ---");
+        System.out.print("Aşı Adı (Örn: Karma, Kuduz): ");
+        String vaccineName = scanner.nextLine();
+        System.out.print("Bir Sonraki Aşı Tarihi (Örn: 10.05.2027): ");
+        String nextDueDate = scanner.nextLine();
 
-		Animal hayvan = null;
+        Vaccine vaccine = new Vaccine(vaccineName, date, nextDueDate, hayvan);
+        System.out.println("AŞI KAYDI BAŞARIYLA OLUŞTURULDU: " + vaccine);
+    }
 
-		if (tur == 1) {
-			System.out.print("Eğitimi var mı? (true)Evet ,(false)Hayır: ");
-			boolean trainingStatus = scanner.nextBoolean();
-			scanner.nextLine();
-			hayvan = new Dog(name, breed, age, weight, height, owner, trainingStatus);
+    //4. SİSTEM: Kayıtlı Hayvanları Göster
+    private static void hayvanlarıGoster() {
+        if (hayvanSayisi == 0) {
+            System.out.println("Henüz kayıtlı hayvan bulunmamaktadır.");
+            return;
+        }
+        System.out.println("\n--- KAYITLI HAYVANLAR ---");
+        for (int i = 0; i < hayvanSayisi; i++) {
+            System.out.println("\n" + (i + 1) + ". Hayvan:");
+            hayvanlar[i].displayInfo();
+        }
+    }
 
-			System.out.println(""
-					+ "   __\r\n"
-					+ " o-''|\\_____/)\r\n"
-					+ "  \\_/|_)     )\r\n"
-					+ "     \\  __  /\r\n"
-					+ "     (_/ (_/");
-			hayvan.register();
-			hayvan.displayInfo();
-		} else if (tur == 2) {
-			System.out.print("Yaşam Alanı (true)Ev Kedisi , (false)Sokak Kedisi: ");
-			boolean isIndoor = scanner.nextBoolean();
-			System.out.print("Kısırlaştırıldı mı? (true)Evet ,(false)Hayır : ");
-			boolean isNeutered = scanner.nextBoolean();
-			scanner.nextLine();
-			hayvan = new Cat(name, breed, age, weight, height, owner, isIndoor, isNeutered);
-			System.out.println(""
-					+ " /\\_/\\\r\n"
-					+ " ( o.o )\r\n"
-					+ "  > ^ <\r\n"
-					+ " /  |  \\\r\n"
-					+ "/___|___\\");
-			hayvan.register();
-			hayvan.displayInfo();
-		} else if (tur == 3) {
-			System.out.println("Uçabiliyor mu? (true)Evet ,(false)Hayır: ");
-			boolean canFly = scanner.nextBoolean();
-			scanner.nextLine();
-			System.out.println("Gaga Yapısı Nasıl: ");
-			String beakStructure = scanner.nextLine();
-			System.out.println("Tüy Rengi: ");
-			String featherColor = scanner.nextLine();
-			hayvan = new Bird(name, breed, age, weight, height, owner, canFly, beakStructure, featherColor);
-			System.out.println(""
-					+ "   ,_,\r\n"
-					+ "  (O,O)\r\n"
-					+ "  (   )\r\n"
-					+ "  -\"-\"-");
-			hayvan.register();
-			hayvan.displayInfo();
-		} else if (tur == 4) {
-			System.out.print("Su Tipi: ");
-			String waterType = scanner.nextLine();
-			System.out.println("Rengi: ");
-			String color = scanner.nextLine();
-			hayvan = new Fish(name, breed, age, weight, height, owner, waterType, color);
-			System.out.println("><((((º>");
-			hayvan.register();
-			hayvan.displayInfo();
-		} else {
-			System.out.print("Kullanım Amacı (true)Yarış Atı, (false)Çiftlik Atı: ");
-			boolean isRideable = scanner.nextBoolean();
-			scanner.nextLine();
-			hayvan = new Horse(name, breed, age, weight, height, owner, isRideable);
-			System.out.println(""
-					+ "          /|,,,/|\r\n"
-					+ "         {/ ● ¿¿\\\r\n"
-					+ "       { {`\\     \\\\\r\n"
-					+ "      ¿{   \\   • •)\r\n"
-					+ "       ¿ /   `>–'\r\n"
-					+ "      |      \\   \\");
-			hayvan.register();
-			hayvan.displayInfo();
-		}
-		
-		return hayvan;
-	}
+    // Oluşturduğumuz hayvanların özelliklerini doğru sınıflara atamak için
+    private static Animal createAnimal(int tur) {
+        Animal hayvan = null;
+
+        if (tur == 1) {
+            hayvan = Dog.create(scanner, owner);
+        } else if (tur == 2) {
+            hayvan = Cat.create(scanner, owner);
+        } else if (tur == 3) {
+            hayvan = Bird.create(scanner, owner);
+        } else if (tur == 4) {
+            hayvan = Fish.create(scanner, owner);
+        } else {
+            hayvan = Horse.create(scanner, owner);
+        }
+
+        hayvan.register();
+        hayvan.displayInfo();
+
+        // Diziye ekle
+        hayvanlar[hayvanSayisi] = hayvan;
+        hayvanSayisi++;
+
+        return hayvan;
+    }
 }
