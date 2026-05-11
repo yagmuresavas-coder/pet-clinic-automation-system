a15363885f087a0364e009811e4c8623c40bba1a
Yağmur <yagmure.savas@gmail.com>
2026-05-11 19:43:41 +0300

    Implement Horse creation method with user input
    Added a static method to create a Horse instance with user input.

--- src/model/Horse.java
+++ src/model/Horse.java
@@ -9,7 +9,51 @@ public class Horse extends Animal {
 			super(name, breed, age, weight, height,owner);
 			this.isRideable=isRideable;
 		}
+     public static Horse create(Scanner scanner, Owner owner) {
+		        System.out.print("Adı: ");
+		        String name = scanner.nextLine();
+		        System.out.print("Cinsi: ");
+		        String breed = scanner.nextLine();
+		        int age;
+				while (true) {
+				    System.out.print("Yaş: ");
+		     	    age = scanner.nextInt();
+				    if (age >= 0) break;
+				    System.out.println("Hata: Yaş negatif değer olamaz! Tekrar girin.");
+				}
 
+				double weight;
+				while (true) {
+				    System.out.print("Kilo: ");
+				    weight = scanner.nextDouble();
+				    if (weight >= 0) break;
+				    System.out.println("Hata: Kilo negatif olamaz! Tekrar girin.");
+				}
+
+				double height;
+				while (true) {
+				    System.out.print("Boy: ");
+				    height = scanner.nextDouble();
+				    if (height >= 0) break;
+				    System.out.println("Hata: Boy negatif olamaz! Tekrar girin.");
+				}
+		        scanner.nextLine();
+		        System.out.print("Kullanım Amacı (true)Yarış Atı, (false)Çiftlik Atı: ");
+		        boolean isRideable = scanner.nextBoolean();
+		        scanner.nextLine();
+		 
+		        System.out.println("     "
+		        		+ "     /|,,,/|\n    "
+		        		+ "     {/ ● ¿¿\\\n  "
+		        		+ "     { {`\\    "
+		        		+ " \\\\\n    "
+		        		+ "  ¿{   \\   • •)\n   "
+		        		+ "    ¿ /   `>–'\n    "
+		        		+ "  |      \\   \\");
+		 
+		        
+		        return new Horse(name, breed, age, weight, height, owner, isRideable);
+		    }
 	@Override
 	public String getType() {
 		return "At";
